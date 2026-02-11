0004482f31e746f472157642e566947e63582652
superevilstockholm <balilombok11@gmail.com>
2026-02-11 22:57:32 +0700

    Created classroom model and updated student model add relationship with classroom

--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+# Models
+from core.models import TimeStampModel
+
 # Create your models here.
+class Classroom(TimeStampModel):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField()
--- student/models.py
+++ student/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+# Models
 from core.models import TimeStampModel
+from classroom.models import Classroom
 
 # Create your models here.
 class Student(TimeStampModel):
     nisn = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=255)
     dob = models.DateField()
+    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE)
